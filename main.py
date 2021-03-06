3c711de47f18c74a50e645713c3a0d0123bd4d54
JP <mjurasovic@gmail.com>
2021-03-06 11:52:43 +1100

    start of mip_conif solver

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cvxpy as cp
 import mip
+from mip_conif import PythonMIP
 
 
 def run_sample_optimization_cvxopt():
@@ -23,4 +24,25 @@ def run_sample_optimization_cvxopt():
     return product_supply, demand, allocation.value, optimal_value
 
 
+def run_sample_optimization_mip():
+    demand = np.array([[100, 500, 30], [20, 200, 50], [150, 15, 35], [10, 5, 25]])
+    product_supply = np.array([550, 200, 170, 40])
+    allocation = cp.Variable(demand.shape, integer=True)
+    objective = cp.Maximize(cp.sum(allocation/demand))
+    constraints =[cp.sum(allocation, axis=1) <= product_supply,
+                allocation <= demand,
+                allocation >= 0]
+    problem = cp.Problem(objective, constraints)
+
+    optimal_value = problem.solve(solver=PythonMIP())
+
+    print('CVXOPT')
+    print('product supply:', product_supply)
+    print('demand:\n', demand)
+    print('allocation:\n', allocation.value)
+    print('calculated score:', optimal_value)
+    return product_supply, demand, allocation.value, optimal_value
+
+
+run_sample_optimization_mip()
 run_sample_optimization_cvxopt()
