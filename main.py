0c0cc27ca1188c2abf797e047a6724b39fd5f600
JP <mjurasovic@gmail.com>
2021-03-06 11:54:51 +1100

    add a linear variable with an == constraint to help me test

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ def run_sample_optimization_cvxopt():
     demand = np.array([[100, 500, 30], [20, 200, 50], [150, 15, 35], [10, 5, 25]])
     product_supply = np.array([550, 200, 170, 40])
     allocation = cp.Variable(demand.shape, integer=True)
-    objective = cp.Maximize(cp.sum(allocation/demand))
-    constraints =[cp.sum(allocation, axis=1) <= product_supply,
-                allocation <= demand,
-                allocation >= 0]
+    a = cp.Variable(shape=1)
+    objective = cp.Maximize(cp.sum(allocation/demand) + cp.sum(a))
+    constraints =[
+        cp.sum(allocation, axis=1) <= product_supply,
+        allocation <= demand,
+        allocation >= 0,
+        cp.sum(a) == 2,
+    ]
     problem = cp.Problem(objective, constraints)
 
     optimal_value = problem.solve(solver=cp.GLPK_MI)
@@ -28,10 +32,14 @@ def run_sample_optimization_mip():
     demand = np.array([[100, 500, 30], [20, 200, 50], [150, 15, 35], [10, 5, 25]])
     product_supply = np.array([550, 200, 170, 40])
     allocation = cp.Variable(demand.shape, integer=True)
-    objective = cp.Maximize(cp.sum(allocation/demand))
-    constraints =[cp.sum(allocation, axis=1) <= product_supply,
-                allocation <= demand,
-                allocation >= 0]
+    a = cp.Variable(shape=1)
+    objective = cp.Maximize(cp.sum(allocation/demand) + cp.sum(a))
+    constraints =[
+        cp.sum(allocation, axis=1) <= product_supply,
+        allocation <= demand,
+        allocation >= 0,
+        cp.sum(a) == 2,
+    ]
     problem = cp.Problem(objective, constraints)
 
     optimal_value = problem.solve(solver=PythonMIP())
@@ -44,5 +52,5 @@ def run_sample_optimization_mip():
     return product_supply, demand, allocation.value, optimal_value
 
 
-run_sample_optimization_mip()
 run_sample_optimization_cvxopt()
+run_sample_optimization_mip()
