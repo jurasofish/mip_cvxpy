3150cacab1f823a9433b2a588848ec5536beb64a
JP <mjurasovic@gmail.com>
2021-03-13 13:14:44 +1100

    black

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 
-with open('./readme.md', 'r') as ff:
+with open("./readme.md", "r") as ff:
     readme_text = ff.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 # Parse version
@@ -16,18 +16,18 @@ for line in init.read_text().split("\n"):
         version = line.split("=")[-1].strip('"')
         break
 if version is None:
-    raise ValueError('No version found')
+    raise ValueError("No version found")
 
 setup(
-    name='mip-cvxpy',
+    name="mip-cvxpy",
     version=version,
     description="Solve MILP CVXPY problems using python-mip",
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     long_description=readme_text,
-    long_description_content_type='text/markdown',
-    author='Michael Jurasovic',
+    long_description_content_type="text/markdown",
+    author="Michael Jurasovic",
     url="https://github.com/jurasofish/mip_cvxpy",
-    license='MIT License',
+    license="MIT License",
     packages=find_packages(),
     # package_data={'sphinx_toggleprompt': ['_static/toggleprompt.js_t']},
     classifiers=["License :: OSI Approved :: MIT License"],
